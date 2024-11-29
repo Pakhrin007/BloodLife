a580a11e2029c87634cb8cd8e785758155046f41
UshavShrestha <ushavshrest9@gmail.com>
2024-11-29 08:12:43 +0545

    Map added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bloodlife/Dummypages/Bloodrequest.dart';
 import 'package:bloodlife/api/api.dart';
 import 'package:bloodlife/splashscreen/splashscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:bloodlife/pages/maps.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Api(),
+      home: MapPage(),
     );
   }
 }
